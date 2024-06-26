96b1db3937ed75f12679413ba7b1538dcd24680b
SHUBHAM_SANGANI <shubhamsangani5503@gmail.com>
2024-06-26 14:48:48 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("HELLO WORLD")
+
+def greet(name):
+    return f"Hello, {name}!"
+def add(a, b):
+    return a + b
+# Main part of the script
+if __name__ == "__main__":
+    name = "shubham"
+    print(greet(name))
+    num1 = 5
+    num2 = 3
+    print(f"The sum of {num1} and {num2} is {add(num1, num2)}")
